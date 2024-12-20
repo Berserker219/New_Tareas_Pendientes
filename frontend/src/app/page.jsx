a1b95cce58296a19ea846a23edf3df46634f5f66
Berserker219 <jjdavid81@gmail.com>
2024-12-20 17:40:20 -0600

    Agregar frontend como carpeta normal

---
+++ frontend/src/app/page.jsx
@@ -0,0 +1,19 @@
+import FormTask from "./components/FormTask"
+import ListTask from "./components/ListTask"
+export const dynamic = "force-dynamic"
+
+
+function Homepage() {
+  return(
+    <div className="container mx-auto">
+      <h1>Task App</h1>
+      <div className="flex gap-x-10">
+        <FormTask />
+        <ListTask />  
+      </div>
+      
+    </div>
+  )
+}
+
+export default Homepage
